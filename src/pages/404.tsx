c79fd170edb715f74ce6237b435e129d2572b72b
AntonioChagoya <A01731037@itesm.mx>
2023-08-07 20:43:18 -0600

    fix - 404 and 500 errors - missing layout defined

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import { LayoutType } from "types/app"
+
+const Page500 = () => {
+
+  return (
+    <div className="bg-gray-900 flex flex-col items-center justify-center h-[90vh] gap-10">
+      <div className="text-center">
+        <h1 className="text-7xl  text-center font-bold text-gray-100 mb-2">Error 500</h1>
+        <p className="text-2xl font-medium mb-4">Page not found</p>
+      </div>
+      <a href="/" className="block w-auto px-[31px] py-[13px] transition duration-150 ease-in-out delay-150 border rounded-[50px] border-PrimmaryColor h-fit hover:cursor-pointer hover:scale-110 text-PrimmaryColor">Go to home page</a>
+    </div>
+  )
+}
+
+Page500.layout = LayoutType.PUBLIC
+export default Page500
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,6 @@ export async function getStaticProps() {
 }
 
 export default function Home({ products }) {
-  console.log("prods", products);
 
   return (
     <>
